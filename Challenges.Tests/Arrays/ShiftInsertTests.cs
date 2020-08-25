03930a11ffcd1b3b53ef1ab5694f4304ad014783
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-08-25 15:22:57 -0500

    Added unit tests for shift insert method

---
+++ Challenges.Tests/Arrays/ShiftInsertTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Challenges.Arrays;
+using Xunit;
+
+namespace Challenges.Tests.Arrays
+{
+    public class ShiftInsertTests
+    {
+        [Fact]
+        public static void ShiftInserter_insert_value_into_middle_of_even_array()
+        {
+            // Arrange
+            int value = 3;
+            int[] array = new int[4] { 1, 2, 4, 5 };
+
+            // Act
+            int[] result = ShiftInsert.ShiftInserter(array, value);
+
+            // Assert
+            Assert.Equal(new[] { 1, 2, 3, 4, 5 }, result);
+        }
+
+        [Fact]
+        public static void ShiftInserter_inserts_value_into_middle_of_odd_array()
+        {
+            // Arrange
+            int value = 3;
+            int[] array = new int[4] { 1, 2, 4, 5 };
+
+            // Act
+            int[] result = ShiftInsert.ShiftInserter(array, value);
+
+            // Assert
+            Assert.Equal(new[] { 1, 2, 3, 4, 5 }, result);
+        }
+
+        [Fact]
+        public static void ShiftInsert_inserts_new_value_for_length_0_array()
+        {
+            // Arrange
+            int value = 3;
+            int[] array = new int[0];
+
+            // Act
+            int[] result = ShiftInsert.ShiftInserter(array, value);
+
+            // Assert
+            Assert.Equal(new[] { 3 }, result);
+        }
+
+        [Fact]
+        public static void ShiftInsert_inserts_new_value_for_length_1_array()
+        {
+            // Arrange
+            int value = 3;
+            int[] array = new int[1] { 1 };
+
+            // Act
+            int[] result = ShiftInsert.ShiftInserter(array, value);
+
+            // Assert
+            Assert.Equal(new[] { 1, 3 }, result);
+        }
+    }
+}
