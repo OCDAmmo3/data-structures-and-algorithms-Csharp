27aa05be5e8a92baaf8954cedab6ef8523e99413
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-08-26 17:33:16 -0500

    Tests made for Binary Search

---
+++ Challenges.Tests/Arrays/BinarySearchTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Challenges.Arrays;
+using Xunit;
+
+namespace Challenges.Tests.Arrays
+{
+    public class BinarySearchTests
+    {
+        [Fact]
+        public static void Search_should_return_index_of_given_value()
+        {
+            // Arrange
+            int[] array = new int[15] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            int value = 6;
+
+            // Act
+            int result = BinarySearch.Search(value, array);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public static void Search_should_return_negative_1_given_value_that_is_not_in_array()
+        {
+            // Arrange
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            int value = 6;
+
+            // Act
+            int result = BinarySearch.Search(value, array);
+
+            // Assert
+            Assert.Equal(-1, result);
+        }
+    }
+}
