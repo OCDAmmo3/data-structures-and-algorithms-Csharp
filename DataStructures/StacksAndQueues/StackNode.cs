d6cf6b9f07ff032769f4754482c78bc088fb5fd8
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-04 14:18:03 -0500

    Added files for stacks and queues and stack node

---
+++ DataStructures/StacksAndQueues/StackNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.StacksAndQueues
+{
+    class StackNode
+    {
+        public StackNode(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
+
+        public StackNode Next { get; set; }
+    }
+}
