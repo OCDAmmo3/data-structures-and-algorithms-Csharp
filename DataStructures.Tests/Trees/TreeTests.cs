ff23f3cbb4a499d1aa0f300041eb82d299b70916
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-12 13:08:24 -0500

    Added working PreOrder with tests

---
+++ DataStructures.Tests/Trees/TreeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DataStructures.Trees;
+
+namespace DataStructures.Tests.Trees
+{
+    public class TreeTests
+    {
+        public static BinaryTree<string> CreateTree()
+        {
+            BinaryTree<string> tree = new BinaryTree<string>();
+            tree.Root = new BinaryTree<string>.Node("Root");
+            tree.Root.Left = new BinaryTree<string>.Node("RootLeft");
+            tree.Root.Right = new BinaryTree<string>.Node("RootRight");
+            tree.Root.Left.Left = new BinaryTree<string>.Node("RootLeftLeft");
+            tree.Root.Left.Right = new BinaryTree<string>.Node("RootLeftRight");
+            tree.Root.Right.Left = new BinaryTree<string>.Node("RootRightLeft");
+            tree.Root.Right.Right = new BinaryTree<string>.Node("RootRightRight");
+            return tree;
+        }
+
+        [Fact]
+        public void PreOrder_should_return_tree_down_left_then_down_right()
+        {
+            // Arrange
+            BinaryTree<string> tree = CreateTree();
+
+            // Act
+            string result = tree.PreOrder(tree.Root);
+
+            // Assert
+            Assert.Equal("Root, RootLeft, RootLeftLeft, RootLeftRight, RootRight, RootRightLeft, RootRightRight", result);
+        }
+    }
+}
--- DataStructures/Trees/BinaryTree.cs
+++ DataStructures/Trees/BinaryTree.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DataStructures.StacksAndQueues;
 
 namespace DataStructures.Trees
 {
 
-    class BinaryTree<T>
+    public class BinaryTree<T>
     {
         public class Node
         {
@@ -14,13 +12,32 @@ namespace DataStructures.Trees
                 Value = value;
             }
             public T Value { get; set; }
-            public Node left { get; set; }
-            public Node right { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
         }
 
         public Node Root { get; set; }
         public int Length { get; set; }
 
+        public string PreOrder(Node root)
+        {
+            Queue<T> queue = new Queue<T>();
+            queue.Enqueue(root.Value);
+            if (root.Left != null)
+                PreOrder(root.Left, queue);
+            if (root.Right != null)
+                PreOrder(root.Right, queue);
+            return queue.ToString();
+        }
 
+        public string PreOrder(Node root, Queue<T> queue)
+        {
+            queue.Enqueue(root.Value);
+            if (root.Left != null)
+                PreOrder(root.Left, queue);
+            if (root.Right != null)
+                PreOrder(root.Right, queue);
+            return queue.ToString();
+        }
     }
 }
