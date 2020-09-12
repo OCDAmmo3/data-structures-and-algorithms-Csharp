84ad49e443e7a1516ed24345de1e22e90bca1891
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-12 12:24:58 -0500

    Added trees scaffolding

---
+++ DataStructures/Trees/BinaryTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Trees
+{
+
+    class BinaryTree<T>
+    {
+        public class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+            public T Value { get; set; }
+            public Node left { get; set; }
+            public Node right { get; set; }
+        }
+
+        public Node Root { get; set; }
+        public int Length { get; set; }
+
+
+    }
+}
