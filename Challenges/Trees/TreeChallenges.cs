ee02559e1acce971655aad82ebf0e1f8f8f05356
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-16 14:30:21 -0500

    Added initial method for FizzBuzzTree

---
+++ Challenges/Trees/TreeChallenges.cs
@@ -0,0 +1,36 @@
+﻿using DataStructures.LinkedLists;
+using DataStructures.Trees;
+
+namespace Challenges.Trees
+{
+    public class TreeChallenges
+    {
+        public static BinaryTree<string> FizzBuzzify(BinaryTree<int> tree)
+        {
+            if (tree.Root == null)
+                return tree;
+            if (tree.Root.Value.GetType() != typeof(int))
+                throw new NotIntTreeException();
+
+            BinaryTree<string> resultTree = new BinaryTree<string>();
+            BinaryTree<string>.Node newRoot = resultTree.Root;
+
+            BinaryTree<int>.Node root = tree.Root;
+            if (root.Value % 15 == 0)
+                newRoot = new BinaryTree<string>.Node("FizzBuzz");
+            else if (root.Value % 5 == 0)
+                newRoot = new BinaryTree<string>.Node("Buzz");
+            else if (root.Value % 3 == 0)
+                newRoot = new BinaryTree<string>.Node("Fizz");
+            else
+                newRoot = new BinaryTree<string>.Node(root.Value.ToString());
+
+            if (root.Left != null)
+                FizzBuzzify(root.Left, newRoot.Left, resultTree);
+            if (root.Right != null)
+                FizzBuzzify(root.Right, newRoot.Right, resultTree);
+
+            return resultTree;
+        }
+    }
+}
